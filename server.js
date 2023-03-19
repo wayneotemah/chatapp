2af1309bcc0a587564b5208b6b9a0fd84585f10c
Wenslous Egesa <wayneotemahegesa@gmail.com>
2023-03-19 20:26:27 +0300

    inital commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const rooms = {};
+// rooms can be enable private or mulicute communication between nodes
+
+app.get("/", (req, res) => {
+  res.render("index", { rooms: rooms });
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomName: req.params.room });
+});
+
+server.listen(3000);
+
+io.on("connection", (socket) => {
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+    console.log(`${name} connected`);
+  });
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+  socket.on("disconnect", (name) => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
