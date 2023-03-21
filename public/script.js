080665cb026be8ae77bb9ef12a785588b1e733f6
Wenslous Egesa <wayneotemahegesa@gmail.com>
2023-03-21 15:10:44 +0300

    mvp working

--- public/script.js
+++ public/script.js
@@ -1,48 +1,47 @@
-const socket = io("http://localhost:3000");
+const socket = io('http://localhost:3000')
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
 
-const messageForm = document.getElementById("send-container");
-const messageInput = document.getElementById("message-input");
-const messageContainer = document.getElementById("message-container");
-const roomContainer = document.getElementById("room-container");
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  appendMessage('You joined')
+  socket.emit('new-user', roomName, name)
 
-if (messageForm) {
-  const username = prompt("What is your name?");
-  appendMessage("You joined");
-
-  socket.emit("new-user", roomName, username);
-  messageForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const message = messageInput.value;
-    socket.emit("send-chat-message", roomName, message);
-    appendMessage(`You: ${message}`);
-    messageInput.value = "";
-  });
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const message = messageInput.value
+    appendMessage(`You: ${message}`)
+    socket.emit('send-chat-message', roomName, message)
+    messageInput.value = ''
+  })
 }
 
-socket.on("chat-message", (data) => {
-  appendMessage(`${data.name}: ${data.message}`);
-});
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText = room
+  const roomLink = document.createElement('a')
+  roomLink.href = `/${room}`
+  roomLink.innerText = 'join'
+  roomContainer.append(roomElement)
+  roomContainer.append(roomLink)
+})
 
-socket.on("user-connected", (name) => {
-  appendMessage(`${name} connected`);
-});
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
 
-socket.on("user-disconnected", (name) => {
-  appendMessage(`${name} disconnected`);
-});
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
 
-socket.on("room-created", (room) => {
-  const roomElement = document.createElement("div");
-  roomElement.innerText = room;
-  const roomLink = document.createElement("a");
-  roomLink.href = `/${room}`;
-  roomLink.innerText = "Join";
-  roomContainer.append(roomElement);
-  roomContainer.append(roomLink);
-});
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
 
 function appendMessage(message) {
-  const messageElement = document.createElement("div");
-  messageElement.innerText = message;
-  messageContainer.append(messageElement);
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
 }
--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-const rooms = { name: { users: {} } };
+const rooms = {};
 // rooms can be enable private or mulicute communication between nodes
 
 app.get("/", (req, res) => {
-  return res.render("index", { rooms: rooms });
+  res.render("index", { rooms: rooms });
 });
 
 app.post("/room", (req, res) => {
@@ -21,23 +21,25 @@ app.post("/room", (req, res) => {
   }
   rooms[req.body.room] = { users: {} };
   res.redirect(req.body.room);
-  return io.emit("room-created", req.body.room);
+  io.emit("room-created", req.body.room);
 });
 
 app.get("/:room", (req, res) => {
-  // console.log(rooms[req.params.room]);
+  // console.log(rooms[req.params.room])
   if (!rooms[req.params.room]) {
     return res.redirect("/");
   }
-  return res.render("room", { roomName: req.params.room });
+  res.render("room", { roomName: req.params.room });
 });
+
+server.listen(3000);
 
 io.on("connection", (socket) => {
   socket.on("new-user", (room, name) => {
     console.log(room);
     socket.join(room);
     rooms[room].users[socket.id] = name;
-    socket.to(room).emit("user-connected", name); //or socket.broadcast.to(room).emit("user-connected", name);
+    socket.to(room).emit("user-connected", name); //or socket.broadcast.to(room).emit("user-connected", name)
     console.log(`${name} connected`);
   });
   socket.on("send-chat-message", (room, message) => {
@@ -60,5 +62,3 @@ function getUserRooms(socket) {
     return names;
   }, []);
 }
-
-server.listen(3000);
